7450c510edc1da4799ae563197455a24b77f2e35
Margarita Kharokhorina <kharokhorina_mb@mail.ru>
2023-04-06 15:31:50 +0300

    add new files

--- seminar/Main.java
+++ seminar/Main.java
@@ -4,19 +4,52 @@ import units.*;
 
 import java.util.ArrayList;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        ArrayList<BaseHero> heroes = new ArrayList<>();
-        heroes.add(new Sniper("Vero"));
-        heroes.add(new Crossbowman("Ron"));
-        heroes.add(new Mage("Aver"));
-        heroes.add(new Monk("Atmir"));
-        heroes.add(new Spearman("Voland"));
-        heroes.add(new Thief("Roman"));
-        heroes.add(new Peasant("Ashet"));
-
+        ArrayList<BaseHero> heroes = rndClassesArrList();
         heroes.forEach(n -> System.out.println(n));
-
-
     }
+
+    private static String getName() {
+        return Names.values()[new Random().nextInt(Names.values().length)].toString();
+    }
+
+    private static ArrayList<BaseHero> rndClassesArrList(){
+        ArrayList<BaseHero> heroes = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            int random = new Random().nextInt(6);
+            switch (random){
+                case 0: heroes.add(new Bowman(getName()));
+                break;
+                case 1: heroes.add(new Crossbowman(getName()));
+                break;
+                case 2: heroes.add(new Mage(getName()));
+                break;
+                case 3: heroes.add(new Monk(getName()));
+                break;
+                case 4: heroes.add(new Spearman(getName()));
+                break;
+                case 5: heroes.add(new Thief(getName()));
+                break;
+                default: heroes.add(new Peasant(getName()));
+            }
+        }
+        return heroes;
+    }
+
 }
+       // heroes.add(new Sniper("Vero"));
+       // heroes.add(new Crossbowman("Ron"));
+        //heroes.add(new Mage("Aver"));
+        //heroes.add(new Monk("Atmir"));
+        //heroes.add(new Spearman("Voland"));
+        //heroes.add(new Thief("Roman"));
+        //heroes.add(new Peasant("Ashet"));
+
+       // heroes.forEach(n -> System.out.println(n));
+
+
+    //}
+//}
---
+++ units/Bowman.java
@@ -0,0 +1,16 @@
+package units;
+
+public class Bowman extends Shooter {
+    int arrows;
+
+    int accuracy;
+
+    public Bowman(String name) {
+        super(100, name, 1, 6, 50, new int[]{6, 12},  10, 60);
+    }
+
+    @Override
+    public String getInfo() {
+        return "Лучник";
+    }
+}
