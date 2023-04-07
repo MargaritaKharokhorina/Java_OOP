cb72786f2c03224be61e87f3c7bfcbd0e5474fef
Margarita Kharokhorina <kharokhorina_mb@mail.ru>
2023-04-07 09:01:31 +0300

    another additions for bowman

--- units/Bowman.java
+++ units/Bowman.java
@@ -2,9 +2,6 @@ package units;
 
 
 public class Bowman extends Shooter {
-    int arrows;
-
-    int accuracy;
 
     public Bowman(String name, boolean firstTeam) {
         super(100, name, firstTeam, 50, new int[]{6, 12},  10, 60);
